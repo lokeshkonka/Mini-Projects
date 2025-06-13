f37ba0452d4db95e83d3c947bedfa19cc6238e5e
Konka Lokesh <konkalokesh372@gmail.com>
2025-06-13 17:19:51 +0530

    javascript add

---
+++ Expense-tracker/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const expenseform = document.getElementById('expense-form')
+const expensename = document.getElementById('expense-name')
+const expenseamount = document.getElementById('expense-amount')
+const expenselist = document.getElementById('expense-list')
+const totalamountdisplay = document.getElementById('total')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
